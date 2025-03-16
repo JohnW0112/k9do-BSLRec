8d08f2fa51faec1a89208b82b35f5f191204679c
JohnW0112 <eldonwong123456@gmail.com>
2025-03-16 16:41:37 +0000

    init: project setup

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # k9do-BSLRec
+k9do-BSLRec aims to implement BSL (British Sign Language) into Pepper. Pepper will then perform a series of action based on recognized signs.
+
+## Remark (Dev)
+- The `process.py` should be working as is, awaiting implementation on ML BSL recognition. This is where the video feed from Pepper is received and processed with the BSL model. Outputs a character when sign is recognized and sent to `pepper_control.py`.
+- `pepper_control.py` functions are to be done. Responsible for sending video feed and performing actions based on input from `process.py`.
+
+## Requirements
+**Python 3** packages required:
+- opencv2
+- numpy
+
+**Python 2.7** packages required:
+- Numpy (1.16.6)
+- naoqi
+> To install the naoqi package, please go to the official website for the [installation guide](https://www.bx.psu.edu/~thanh/naoqi/dev/python/install_guide.html)
+
+***Installing Packages:***
+
+```bash
+#python2
+python2 -m pip install numpy==1.16.6
+
+#python3
+python3 -m pip install numpy opencv-python
+```
+
+## Usage
+Both `process.py` and `pepper_control.py` requires different python versions in order to work. Virtual environments could be setup to run them seperately but the following is also fine.
+
+```bash
+#Run this first
+python2 pepper_control.py
+
+#then this
+python3 process.py
+
+```
+
+## Resources
+- [Documentation](http://doc.aldebaran.com/2-1/naoqi/index.html) on how to use naoqi api
+- A detailed video on how to program Pepper and port code from Choregraphe can be found [here](https://www.youtube.com/watch?v=iAeis7j5LmE).
---
+++ pepper_control.py
@@ -0,0 +1,147 @@
+#! /usr/bin/python2
+
+import socket
+import struct
+import numpy as np
+from naoqi import ALProxy
+
+# Pepper's IP and Port
+PEPPER_IP = "192.168.0.109"  # Pepper IP
+PORT = 9559
+
+'''PARAMETERS'''
+# Subscribe to the video feed
+resolution = 3              # 1280x960
+color_space = 13            # BGR color space
+fps = 30                    # max fos = 30
+
+# Set up a server socket to receive messages
+HOST = '127.0.0.1'  # Localhost
+PORT_CONTROL = 5005  # For getting command
+PORT_VIDEO = 5006 # For sending video feed
+server_socket_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket_con.bind((HOST, PORT_CONTROL))
+server_socket_con.listen(1)
+
+print("Waiting for control connection from process.py...")
+conn_con, addr_con = server_socket_con.accept()
+print("Connected to:", addr_con)
+
+server_socket_vid = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket_vid.bind((HOST, PORT_VIDEO))
+server_socket_vid.listen(1)
+print("Waiting for video feed connection from process.py...")
+conn_vid, addr_vid = server_socket_vid.accept()
+print("Connected to:", addr_vid)
+
+# Create proxy
+'''
+Everything you need from the NAOqi should be defined here. I just imported some basic proxies
+'''
+video_service = ALProxy("ALVideoDevice", PEPPER_IP, PORT)
+posture = ALProxy("ALRobotPosture", PEPPER_IP, PORT)
+behavior = ALProxy("ALBehaviorManager", PEPPER_IP, PORT)
+touch = ALProxy("ALTouch", PEPPER_IP, PORT)
+
+def pepper_tts(word):
+    #TODO: Text-2-speech
+    print("TTS started...")
+
+def pepper_ipadPrint(word):
+    #TODO: Print words onto the ipad
+    print("Printing words")
+
+def pepper_call():
+    #TODO: Call someone according to selected contact and display "Calling ${CONTACT}" output onto ipad.
+    print("Select call contact")
+    while (not data):
+        data = conn_con.recv(1024).decode()
+    
+        if data:
+            if (data == '1'):
+                print("Calling contact 1")
+                pepper_ipadPrint("Calling contact 1")
+                pepper_tts("Calling contact 1")
+                # Contact 1
+            elif (data == '2'):
+                print("Calling contact 2")
+                pepper_ipadPrint("Calling contact 2")
+                pepper_tts("Calling contact 2")
+                # Contact 2
+            elif (data == '3'):
+                print("Calling contact 3")
+                pepper_ipadPrint("Calling contact 3")
+                pepper_tts("Calling contact 3")
+                # Contact 3
+
+def pepper_raiseArm():
+    #TODO: Raise an arm
+    print("Raising arm...")
+    pepper_tts("Hi")
+    
+
+def pepper_sing():
+    #TODO: Sing
+    print("Singing...")
+
+def pepper_summon():
+    #TODO: Pepper respond to the call of its name, and move towards user
+    print("Being summoned...")
+    pepper_tts("On my way")
+
+def pepper_checkTouch():
+    print("Checking for touch...")
+    touched_sensors = touch.getStatus()
+    
+    for sensor in touched_sensors:
+        sensor_name = sensor[0]  # Name of the sensor
+        is_touched = sensor[1]  # 1 if touched, 0 if not
+        
+        if is_touched:
+            pepper_tts("Oh hi there, how can I help you")
+
+            '''
+            if "Head" in sensor_name:
+                tts.say("head")
+            elif "HandLeft" in sensor_name:
+                tts.say("hand left")
+            elif "HandRight" in sensor_name:
+                tts.say("hand right")
+            elif "Bumper" in sensor_name:
+                tts.say("bumper")
+            '''
+
+if __name__=="__main__":
+    try:
+        # Get video feed and send to process.py
+        subscriber_id = video_service.subscribeCamera("pepper_stream", 0, resolution, color_space, fps)
+        image_container = video_service.getImageRemote(subscriber_id)
+
+        while True:
+            if image_container:
+                width, height = image_container[0], image_container[1]
+                array = image_container[6]
+                frame = np.frombuffer(array, dtype=np.uint8).reshape((height, width, 3))
+
+                # Send frame size and data
+                conn_vid.sendall(struct.pack("L", len(frame.tobytes())) + frame.tobytes())
+            data = conn_con.recv(1024).decode()
+            if not data:
+                break
+
+
+            if data == 'c':
+                pepper_call()
+
+            elif data == 'z':
+                pepper_raiseArm()
+
+            elif data == 'f':
+                pepper_sing()
+
+    finally:
+        video_service.unsubscribe(subscriber_id)
+        conn_vid.close()
+        conn_con.close()
+        server_socket_vid.close()
+        server_socket_con.close()
